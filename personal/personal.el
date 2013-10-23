0bf8dadfe772eab0800a22602136ea55621d0a10
magomimmo <mimmo.cosenza@gmail.com>
2013-10-23 11:03:37 +0200

    added personal.el customizations

---
+++ personal/personal.el
@@ -0,0 +1,8 @@
+;;; package --- summary
+
+(load-theme 'solarized-dark t)
+(setq prelude-whitespace nil)
+(setq prelude-guru nil)
+(setq mac-command-modifier 'meta)
+(setq mac-option-modifier 'none)
+
